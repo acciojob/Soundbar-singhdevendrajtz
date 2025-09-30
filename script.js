7f50490d3f3d32861a1902314f76315e4a923d26
singhdevendrajtz <singhdevendrajtz@gmail.com>
2025-09-30 16:42:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 //your JS code here. If required.
-const sounds=["sound1.mp3","sound2.mp3","sound3.mp3","sound4.mp3","sound5.mp3", "sound2.mp3"]
+const sounds=["https://actions.google.com/sounds/v1/foley/hand_claps_close.ogg" ,"https://actions.google.com/sounds/v1/alarms/spaceship_alarm.ogg","https://actions.google.com/sounds/v1/alarms/spaceship_alarm.ogg","https://actions.google.com/sounds/v1/alarms/spaceship_alarm.ogg", "https://actions.google.com/sounds/v1/alarms/alarm_clock.ogg","https://actions.google.com/sounds/v1/alarms/medium_bell_ringing_near.ogg"]
 const buttons=document.querySelectorAll(".btn");
 const container=document.querySelector("#buttons")
 buttons.forEach((x,y)=>{
 	if(y<=5)
 	{
 buttons[y].addEventListener('click',function(e){
-const audio = new Audio(`sounds/${y}`);
-    audio.play();
+	 if (window.currentAudio) {
+    window.currentAudio.pause();
+    window.currentAudio.currentTime = 0; // reset
+  }
+const audio = new Audio();
+	audio.src=`${sounds[y]}`
+	audio.play();
     // store current audio in global variable
     window.currentAudio = audio;
 
@@ -16,9 +21,11 @@ const audio = new Audio(`sounds/${y}`);
 	else if(y==6)
 	{
 		buttons[y].addEventListener('click',function(){
-			
-			 window.currentAudio.pause();
+		console.log("hello")	
+	 if (window.currentAudio) {
+    window.currentAudio.pause();
     window.currentAudio.currentTime = 0; // reset
+  }
 	})
 			}
 	})
